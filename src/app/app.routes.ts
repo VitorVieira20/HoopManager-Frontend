b23ad6bc27f5278cdecda5c352ed5c7850675a04
Vitor Vieira <2078320@student.uma.pt>
2024-06-21 10:56:29 +0100

    ui: add tailwind css to the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
     {
         path: "dashboard/:owner_id",
         component: DashboardComponent
+    },
+    {
+        path: "contacts/:club_id",
+        component: ContactsComponent
     }
 ];
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#A2CDC4',
+        secondary: '#79A89E',
+        accent: '#36C2A2',
+        background: '#4B4B4B',
+        text: '#F1EFEF',
+        textsecondary: '#232F2C',
+      },
+    },
+  },
+  plugins: [],
+} 
