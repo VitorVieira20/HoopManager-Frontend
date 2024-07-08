6c45101a840bba9554e1a5d36e52aab9948edae8
Vitor Vieira <2078320@student.uma.pt>
2024-07-08 23:55:26 +0100

    fix: fixed owner guard, now it can be used in the admin side and in the client side

--- src/app/guards/owner.guard.ts
+++ src/app/guards/owner.guard.ts
@@ -6,8 +6,9 @@ export const OwnerGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state) 
 
   const userId = sessionStorage.getItem('user-id');
   const ownerId = route.paramMap.get('owner_id');
+  const routeUserId = route.paramMap.get('user_id')
 
-  if (userId === ownerId) {
+  if (userId === ownerId || userId === routeUserId) {
     return true;
   } else {
     alert('You do not have permission to access this page.');
